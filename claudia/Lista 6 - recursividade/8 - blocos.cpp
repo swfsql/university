7337842c584af6aeba4ba18a3e9a0b2706277868
Thiago Machado <thy@hotmail.com.br>
2011-11-21 17:30:44 -0200

    8 feito, lista 6, claudia

--- claudia/Lista 6 - recursividade/8 - blocos.cpp
+++ claudia/Lista 6 - recursividade/8 - blocos.cpp
@@ -1,27 +1,72 @@
 #include <iostream>
+#include <cstring>
+#include <cstdio>
 using namespace std;
 
 struct bloco
 {
-   char nome[50];
-   int idade;
-   bloco *prox;
+   char nome[50]; int idade; bloco *prox;
 };
 
 struct lista
-
-int tabuada(int& base, int i = 10)
 {
-    if(i > 0)
-        cout << tabuada(base, i - 1) << " ";
-    return base * i;
+    bloco* topo;
+
+    void empilha(char n[40], int i = 0)
+    {
+        bloco* no = new bloco;
+        strcpy(no->nome, n); no->idade = i;
+        no->prox = topo; topo = no;
+    }
+
+    void mostra(bloco* no)
+    {
+        if(!no) return;
+        cout << no->nome << " - " << no->idade << "\n";
+        mostra(no->prox);
+    }
+};
+
+int menu ()
+{
+    const int tam = 3;
+    char ops[tam][40] = {"encerrar", "add", "mostrar"};
+
+    int op = -1;
+    cout << "\n\n";
+    while(++op < tam)
+        cout << "\t" << op << ". " << ops[op] << "\n";
+    cin >> op;
+    cout << "\n";
+    return op;
 }
 
 int main()
 {
-    int base;
-    cout << "insira um numero base: ";
-    cin >> base;
-    cout << tabuada(base);
+    int op = menu(),
+        i;
+    char n[50];
+
+    lista l;
+    l.topo = 0;
+
+    while(op)
+    {
+        switch (op)
+        {
+        case 1:
+            cout << "insira o nome: "; fflush(stdin);
+            gets(n);
+            cout << "insira a idade: ";
+            cin >> i;
+            l.empilha(n, i);
+            break;
+
+        case 2:
+            cout << "cadastros: \n\n"; l.mostra(l.topo);
+            break;
+        }
+        op = menu();
+    }
     return 0;
 }

f3f052a7ec063262e726f8a095e1bb677efad61a
Thiago Machado <thy@hotmail.com.br>
2012-03-13 23:55:44 -0300

    git tree updt

--- eco-029/rafael/13 mar/1/1.cpp
+++ eco-029/rafael/13 mar/1/1.cpp
@@ -20,20 +20,20 @@ struct Tree {
         head = 0;
     }
 
+    // adiciona um elemento. (TODO? não deixar ele ser repetido)
     Tree * add(int x) {
         No **no = &head;
         while(*no) no = &(*no)->links[(*no)->key < x];
-        No *newNo = new No(x);
-        *no = newNo;
+        *no = new No(x);
         return this;
     }
 
-    // (sem parâmetros) = (mostrar todos)
+    // mostrar elementos
     Tree * show() {
         show(head);
         return this;
     }
-    // overload function
+    // overload function (mostrar o elemento e os abaixo)
     Tree * show(No *p) {
         if(!p) return this;
         show(p->links[0]); // left
@@ -42,15 +42,14 @@ struct Tree {
         return this;
     }
 
-    // (sem parâmetros) = (remover todos)
+    // remover elementos
     Tree * rm() {
         rm(head);
         return this;
     }
-    // overload function
+    // overload function (remover elemento e os abaixo)
     Tree * rm(No *p) {
         // TODO: remover elementos recursivamente
-
     }
 
 };
@@ -58,7 +57,7 @@ struct Tree {
 int main() {
 
     Tree *tree= new Tree();
-    tree->add(8)->add(6)->add(4)->add(5)->show();
+    tree->add(8)->add(6)->add(4)->add(5)->show(); // por isso que todas as funções retornam a &struct. pra voltar ao normal, botar tudo void.
 
     return 0;
 }

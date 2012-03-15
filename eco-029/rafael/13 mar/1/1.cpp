80601ad124e510c9522f1019e2e1c847f8cea2bc
Thiago Machado <thy@hotmail.com.br>
2012-03-15 10:17:36 -0300

    rm progredindo

--- eco-029/rafael/13 mar/1/1.cpp
+++ eco-029/rafael/13 mar/1/1.cpp
@@ -51,11 +51,11 @@ struct Tree {
         return this;
     }
     // overload function
-    Tree * show(No *p) {
-        if(!p) return this;
-        show(p->links[0]); // left
-        cout << p->key;
-        show(p->links[1]); // right
+    Tree * show(No *no) {
+        if(!no) return this;
+        show(no->links[0]); // left
+        cout << no->key;
+        show(no->links[1]); // right
         return this;
     }
 
@@ -71,20 +71,48 @@ struct Tree {
     }
     // overload function
     Tree * rm(No **no) {
+
+        // debug
         cout << "\n\ncheguei. \nno: " << no;
         cout << "\n*no: " << *no;
         if(*no) cout << "\n(*no)->key: " << (*no)->key;
         cout << "\n\n";
-        // TODO: ajustar ponteiros, deletar nó.
+
+
         int r = rand() % 2; // 0, 1
+        if (!*no) return this; // nó não existe
         No **links = (*no)->links;
-        //if (*links) links = &(*links)[r].links;
-        //while(*links && (*links)[1-r] && (*links)[1-r].links[1-r]) links = &(*links)[1-r].links;
-        cout << "primeira etapa";
+        if (!*links) {
+            // nó é folha
+            *no = 0;
+            return this;
+        }
+
+        // TODO: remover nó que não é folha
+
+        links = (*links)[r].links;
+        cout << (links);
+        cout << "\n";
+        cout << (*links);
+        /*while(*links
+              &&
+              (*links)[1-r]
+              &&
+              (*links)[1-r].links[1-r])
+            links = (*links)[1-r].links;
+        cout << "primeira etapa";*/
 
         return this;
     }
 
+    /*
+    Tree * add(int x) {
+        No **no = &head;
+        while(*no) no = &(*no)->links[(*no)->key < x];
+        *no = new No(x);
+        return this;
+    }
+    */
 
     // procura nó que tenha a chave
     No ** search (int x) {
@@ -123,7 +151,10 @@ int main() {
      */
 
     Tree *tree= new Tree();
-    tree->add(5)->add(3)->add(2)->add(4)->show();//->rm(6)->rm(4); // por isso que todas as funções retornam a &struct. pra voltar ao normal, botar tudo void
+    tree->add(5)->add(3)->add(2)->add(4)->show();
+    cout << "\n\n";
+    tree->rm(2);//->rm(4);
+    tree->show();
     return 0;
 }
 

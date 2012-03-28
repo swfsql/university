64a98e7e2d70dd105c667926c936f521ffa3b990
Thiago Machado <thy@hotmail.com.br>
2012-03-28 11:10:45 -0300

    documentation upd

--- eco-030/calculadora/Expression.java
+++ eco-030/calculadora/Expression.java
@@ -1,9 +1,33 @@
 public class Expression {
 
+	/*
+	 *
+	 *
+	 *
+	 */
+
+	private String _input;
+
 	public Expression(){
+		_input = "";
 
 
+
+
+
+	}
+
+	public void setInput(String s) {
 		
+		_input = s;
+		if (s.equals("") || s.equals("exit")) return;
+	}
+
+	public String getInput() {
+		return _input;
+	}
+	public Boolean getInput(String s) {
+		return _input.equals(s);
 	}
 
 
--- eco-030/calculadora/Main.java
+++ eco-030/calculadora/Main.java
@@ -1,11 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
+
+	/*
+	 * @author:
+	 * - Thiago Machado da Silva (21215).
+	 * - Thiago Paixão Visconti (22233).
+	 * 
+	 * patterns:
+	 * - i/o on Main class only.
+	 * - use _ in private variables or functions.
+	 * - { (opening brackets) on same line.
+	 * 
+	 * about:
+	 * - teacher: Fernando (Universidade Federal de Itajubá, Campus Itabira).
+	 * - text editor: Sublime text 2.
+	 * - compiler: javac.
+	 *
+	 * instructions:
+	 * - type 'exit' to end the program.
+	 * - type '123 + 123' to recive '246'. (TODO). 
+     * - - minimum pattern: 'number operator number'. (TODO)
+     * - - expression pattern: just any supported expression. (TODO - extra)
+	 * - - - type 'A = 123' 'A + 123' to recive '246'. (TODO - extra)
+	 */
 
 
 	public static void main(String[] args) {
-
-		Calculator calc = new Calculator(); // irá calcular a expressão RPN
 		Expression exp = new Expression(); // receberá os inputs e o transformará em uma expressão RPN
-		Var vars = new Var(); // será o head da lista de variáveis
+		Calculator calc = new Calculator(); // irá calcular a expressão RPN
+		Var vars = new Var(); // será o head da lista de variáveis, usadas na expressão e calculadora
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("'exit' quits");
+
+
+		while(!exp.getInput("exit")){
+			System.out.println("");
+			exp.setInput(scan.nextLine());
+		}
 
 
 		

689da4d126f0c5bf56afb23562ae06d03627399c
Thiago Machado <thy@hotmail.com.br>
2012-04-10 19:18:02 -0300

    2th list 7th ex c)

--- estatistica/modelo binominal/js.js
+++ estatistica/modelo binominal/js.js
@@ -3,18 +3,22 @@ function f(n) {
   return (!n || n==1) ? 1 : (n * f(n-1) );
 }
 
+// binomio
+function bin(a, b) {
+  return f(a) / (f(b) * f(a-b));
+}
+
 // modelo binomial
 var n, p
-function b(k) {
-  var d = n-k;
-  return f(n)/(f(k)*f(d)) * Math.pow(p, k) * Math.pow(1-p, d);
+function mb(k) {
+  return bin(n, k) * Math.pow(p, k) * Math.pow(1-p, n-k);
 }
 
 /* exemple 2th list, 2th exercise, c) */
 
 n = 15;
 p = .4;
-b(4) + b(5); // returns 0.31271364801331214, wich is correct
+mb(4) + mb(5); // returns 0.31271364801331214
 
 /* exemple 2th list, 3th exercise, c) */
 
@@ -24,4 +28,8 @@ n = 15;
 p = .8;
 i = 9;
 soma = 0;
-while(++i < n+1) soma += b(i); // returns 0.9389485703823368, wich is correct
+while(++i < n+1) soma += mb(i); // returns 0.9389485703823368
+
+/* example 2th list, 7th exercise, c) */
+
+bin(4,1) * bin(5,2) / bin(9,3) + bin(4,2) * bin(5,1) / bin(9,3); // returns 0.8333333333333333

4022554f452221fa33692fb098d5cf06f8d6f06a
Thiago Machado <thy@hotmail.com.br>
2012-04-10 18:04:16 -0300

    modelo binomial fix

--- estatistica/modelo binominal/js.js
+++ estatistica/modelo binominal/js.js
@@ -1,22 +1,27 @@
+// factorial
 function f(n) {
-  if ((n == 0) || (n == 1))
-    return 1
-   else {
-      result = (n * f(n-1) )
-      return result
-   }
+  return (!n || n==1) ? 1 : (n * f(n-1) );
 }
 
-var n
-var p
-
+// modelo binomial
+var n, p
 function b(k) {
   var d = n-k;
   return f(n)/(f(k)*f(d)) * Math.pow(p, k) * Math.pow(1-p, d);
 }
 
-/* example 2th list, 2th exercise, c) */
+/* exemple 2th list, 2th exercise, c) */
 
-n = 15
-p = .4
-b(4) + b(5) // returns 0.31271364801331214, wich is correct
+n = 15;
+p = .4;
+b(4) + b(5); // returns 0.31271364801331214, wich is correct
+
+/* exemple 2th list, 3th exercise, c) */
+
+var i, soma;
+
+n = 15;
+p = .8;
+i = 9;
+soma = 0;
+while(++i < n+1) soma += b(i); // returns 0.9389485703823368, wich is correct

648e1af58e36185cca789edc422605603d9163dd
Thiago Machado <thy@hotmail.com.br>
2012-04-10 22:23:51 -0300

    poisson

--- estatistica/modelo binominal/js.js
+++ estatistica/modelo binominal/js.js
@@ -14,6 +14,12 @@ function mb(k) {
   return bin(n, k) * Math.pow(p, k) * Math.pow(1-p, n-k);
 }
 
+// modelo poisson
+var y;
+function poisson (k) {
+  return Math.pow (Math.E, -y) * Math.pow (y, k) / f(k);
+}
+
 /* exemple 2th list, 3th exercise, c) */
 
 n = 15;
@@ -33,3 +39,8 @@ while(++i < n+1) soma += mb(i); // returns 0.9389485703823368
 /* example 2th list, 7th exercise, c) */
 
 bin(4,1) * bin(5,2) / bin(9,3) + bin(4,2) * bin(5,1) / bin(9,3); // returns 0.8333333333333333
+
+/* example 2th list, 9th exercise, a) */
+
+y = 5;
+1 - (poisson (0) + poisson (1) + poisson (2)); // returns 0.8753479805169189

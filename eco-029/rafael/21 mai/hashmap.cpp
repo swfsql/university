b3a79ed89dd2e7f9b66289bbaf6d54b0a867570c
Thiago Machado <thy@hotmail.com.br>
2012-05-20 10:23:28 -0300

    hashmap quase pronto.

--- eco-029/rafael/21 mai/hashmap.cpp
+++ eco-029/rafael/21 mai/hashmap.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
 using namespace std;
 
 struct No
@@ -8,7 +11,7 @@ struct No
 	No (int chave, char nome[40]) {
 		next = 0;
 		ra = chave;
-		strcpy(n, nome); 
+		strcpy(n, nome);
 	}
 
 	int ra; // chave
@@ -81,10 +84,46 @@ struct Hashmap {
 
 
 
+struct Iterator
+{
+    No **iter;
+    int j;
+
+    No **getNos(Hashmap *hm)
+    {
+        int i = -1, l = hm->size();
+        iter = new No*[l];
+        j = -1;
+        while(++i<hm->L) getr(hm->hash[i]->next);
+        std::sort(iter,iter+l, Comparador());
+        return iter;
+    }
+    
+    void getr(No *no)
+    {
+        if (!no) return;
+		getr(no->next);
+		iter[++j] = no;
+    }
+
+    void show(int l) {
+    	int i = -1;
+    	while(++i < l) cout << iter[i]->ra << " ";
+    }
+    
+    // http://paulsolt.com/2009/01/stl-pointers-objects-and-sorting/
+    struct Comparador { bool operator() (No* i,No* j) { return i->ra < j->ra; }};
+};
+
+
+
+
+
+
 int main()
 {
 	Hashmap *hm = new Hashmap(7);
-	char nome[40]; 
+	char nome[40];
 	No *aluno;
 
 	//
@@ -140,12 +179,30 @@ int main()
 	cout << "lista esta vazia? " << hm->isEmpty() << "\n";
 	cout << "qual o tamanho dela? " << hm->size() << "\n";
 
-	cout << "\nclear()"; hm->clear();
+	//cout << "\nclear()"; hm->clear();
 
 	cout << "\nlista esta vazia? " << hm->isEmpty() << "\n";
 	cout << "qual o tamanho dela? " << hm->size() << "\n";
 
 	cout << "mostre o nome do aluno 16: " << (hm->get(16)? hm->get(16)->n : "nao") << "\n";
+
+	hm->put(2, new No(2, "aluno 2"));
+	hm->put(3, new No(3, "aluno 3"));
+	hm->put(4, new No(4, "aluno 4"));
+	hm->put(5, new No(5, "aluno 5"));
+	hm->put(6, new No(6, "aluno 6"));
+	hm->put(7, new No(7, "aluno 7"));
+	hm->put(8, new No(8, "aluno 8"));
+	hm->put(9, new No(9, "aluno 9"));
+	
+	cout << "adicionando alunos com ra: 2,3,4,5,6,7,8,9\n\n";
+
+	cout << "num. de elementos: " << hm->size() << "\n";
+
+	Iterator* I = new Iterator();
+	I->getNos(hm);
+	cout << "\no iterador vale: \n";
+	I->show(hm->size());
 
 
 	return 0;

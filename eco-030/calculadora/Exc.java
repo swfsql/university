b05eda49fd35016f54c19c61be3d805f534c95c9
Thiago Machado <thy@hotmail.com.br>
2012-06-18 01:38:07 -0300

    sepa arrumo

---
+++ eco-030/calculadora/Exc.java
@@ -0,0 +1,7 @@
+public class Exc extends Exception {
+	public void divideByZero(String s) throws Exception {
+		int i = -1, l = s.length();
+		while(++i < l) if (s.charAt(i) != '-' && s.charAt(i) != '0') return;
+		throw new Exception("error: divide by zero.");
+	}
+}

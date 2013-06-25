7a3101e0be4a6e0046963dfa62fffff8d5697a31
Thiago Machado <thy@hotmail.com.br>
2013-06-25 09:34:49 +0000

    booth ok

--- arquitetura/booth/main.cpp
+++ arquitetura/booth/main.cpp
@@ -2,8 +2,10 @@
 #include <bitset>
 using namespace std;
 
+#define DEBUG 
+
+
 void pr(bitset <15> bs) {
-    
   cout << "\nS C M    A    Q    Q-1";
   cout << "\n" << bs[14] << " " << bs[13] << " ";
   int i  = -1;
@@ -34,7 +36,7 @@ int main() {
     // 0 0 0000 0000 0000 0
     bs.reset();
 
-    pr(bs); cout << "reset\n";
+    DEBUG pr(bs); cout << "reset\n";
 
     // recebe M e Q.
     {
@@ -52,7 +54,7 @@ int main() {
       bs |= (Q & 15) << 1;
     }
 
-    pr(bs); cout << " M e Q\n";
+    DEBUG pr(bs); cout << " M e Q\n";
 
     // resolvi fazer por esta iteracao constante.
     int i = -1; 
@@ -66,10 +68,10 @@ int main() {
         // 0  1   +
         // 1  0   -
         // 1  1   x
-        bs.set(13, bs[0]); // o Carrier recebe o valor de Q-1 (A_M).
+        bs.set(13, ~bs[0]); // o Carrier recebe o valor de -Q-1 (A_M).
 
         
-    pr(bs); cout << " arrumou carrier\n";
+        DEBUG pr(bs); cout << " arrumou carrier\n";
 
 
         int j = -1; 
@@ -88,26 +90,26 @@ int main() {
           // 1 1 1 | 1 1
           
           // s = A ^ (M ^ A_M ) ^ C. depois A recebe esse s.
-          bs.set(14, bs[j + 5] ^ bs[j + 9] ^ bs[0] ^ bs[13]);
+          bs.set(14, bs[j + 5] ^ bs[j + 9] ^ ~bs[0] ^ bs[13]);
 
           // c = A(M ^ A_M) | CA | C(M ^ A_M). depois C recebe esse c.
-          bs.set(13, (bs[j + 5] & ( bs[j + 9] ^ bs[0])) | 
+          bs.set(13, (bs[j + 5] & ( bs[j + 9] ^ ~bs[0])) | 
               (bs[13] & bs[j + 5]) 
-              | (bs[13] & (bs[j + 9] ^ bs[0])));
+              | (bs[13] & (bs[j + 9] ^ ~bs[0])));
           
           // agora o A recebe de verdade o s
           bs.set(j + 5, bs[14]);
 
-    pr(bs); cout << " fim da soma\n";
+          DEBUG pr(bs); cout << " fim da soma\n";
         }
       }
 
       // shift.
       //
       //
-      pr(bs); cout << "antes do shift\n";
+      DEBUG pr(bs); cout << "antes do shift\n";
       bs >>= 1;
-      pr(bs); cout << "shift\n";
+      DEBUG pr(bs); cout << "shift\n";
 
       // como so' agora que percebi que o proprio M leva shift, tenho que
       // fazer com que ele retorne ao seu estado original.
@@ -116,11 +118,16 @@ int main() {
       bs ^= bs ^ ((bs << 6) >> 6) ^ ((bs >> 8) << 9);
       //((bs & 3840) << 1);
       bs.set(8, bs[7]); // A4 depois do shift sera igual ao A4 de antes.
-      pr (bs); cout << "M arrumado\n";
+      DEBUG pr (bs); cout << "M arrumado\n";
     }
   }
 
-  pr(bs); cout << " fim\n";
+  DEBUG pr(bs); cout << " fim\n";
+  cout << "\nresultado (binario):\n";
+  int i = -1;
+  while(++i < 8) cout << bs[8 - i];
+  cout << "\nresultado (decimal):\n";
+  cout << "todo";
   return 0;
 }
 

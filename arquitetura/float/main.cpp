fcb71b26685a9fcf37db9335b9dbd9c150214114
Thiago Machado <thy@hotmail.com.br>
2013-06-29 19:22:34 +0000

    boa parte dos exemplos funcionam

--- arquitetura/float/main.cpp
+++ arquitetura/float/main.cpp
@@ -5,12 +5,10 @@
 #include <bitset>
 using namespace std;
 
-int main() {
 
-  string input;
-  {
-    cin >> input;
+const bool debug = false;
 
+float converteNum(string input) {
     unsigned int a = 0;
 
     // 0 00000000 00000000000000000000000
@@ -27,99 +25,123 @@ int main() {
     int j = 0;
 
     if (inteiro_i != 0) {
-      cout << "\ninteiro: " << inteiro_i << "\n";
+     if (debug)  cout << "\ninteiro: " << inteiro_i << "\n";
 
-      int inteiro_i2 = inteiro_i;
+      long int inteiro_i2 = inteiro_i;
       while(inteiro_i2 >>= 1) ++j;
       // j = expoente nao polarizado
       
-      cout << "j: " << j << "\n";
+      if (debug)  cout << "j: " << j << "\n";
 
 
-      cout << "partiu significando\n";
-      a += (inteiro_i << (23 - j)) & ((1 << 23)-1);
+      if (debug)  cout << "partiu significando\n";
+      if (debug)  cout << "j: " << j << "\n";
+      int shift = 23 - j;
+      if (shift < 0) a += (inteiro_i >>  (j - 23)) & ((1 << 23)-1);
+      else a += (inteiro_i << (23 - j)) & ((1 << 23)-1);
 
-
-    cout << "\n-----\n";
-    cout << "se     1.s                     .\n";
-    cout << bitset <32> (a) << "\n";
+    if (debug)  cout << "\n-----\n";
+    if (debug)  cout << "se     1.s                     .\n";
+    if (debug)  cout << bitset <32> (a) << "\n";
     }
 
 
     { // agora cuidar dos digitos quebrados
 
       // considerando que nao ha inteiro; ex. "-.5", e a entrada eh valida..
-      cout << "\ndecimal\n"; 
+      if (debug)  cout << "\ndecimal\n"; 
       string quebrado_s = "";
       int k = i;
+      if (debug)  cout << "vai verificar char: " << input[k+1] << "\n";
       while(input[++k] != '0' && input[k]){
         quebrado_s += input[k];
-        cout << "add " << input[k] << "\n";
+        if (debug)  cout << "add " << input[k] << "\n";
 
       }
       quebrado_s += input[k];
       int k2 = k;
+      if (debug)  cout << "agora vai verificar o char: " << input[k2+1] << "\n";
       while(input[++k2]) {
         quebrado_s += input[k2];
-        cout << "add2 " << input[k2] << "\n";
+        if (debug)  cout << "add2 " << input[k2] << "\n";
       }
       long int quebrado_i = atoi(quebrado_s.c_str());
       long int quebrado_i2 = quebrado_i;
-      cout << "\ndecimal: " << quebrado_i << "\n";
-      long int lim = pow(10, k2 - i - 2);
-      cout << "k: " << k << ", k2: " << k2 << ", i: " << i << ", lim: " << lim << "\n";
+      if (debug)  cout << "\ndecimal: " << quebrado_i << "\n";
+      long int lim = pow(10, i? i : k2 - k);
+      if (debug)  cout << "k: " << k << ", k2: " << k2 << ", i: " << i << ", lim: " << lim << "\n";
 
       int l = 0;
       bool fl_primeiro = j != 0;
       int offset = j;
       while(quebrado_i2 *= 2) {
-        cout << "\nquebrado agora: " << quebrado_i2 << "\n";
-        cout << "++l: " << l+1 << ", j: " << j << "\n";
-        if(++l >= 22 || l + j >= 22) {
-          cout << "son the bit, l: " << l << ", j: " << j << "\n";
+        if (debug)  cout << "\nquebrado agora: " << quebrado_i2 << "\n";
+        if (debug)  cout << "++l: " << l+1 << ", j: " << j << "\n";
+        ++l;
+        if (debug)  cout << "l: " << l << ", offset: " << offset << ", j: " << j << "\n" ;
+          if (debug)  cout << "vamo ve se vai sair: " << l + (j > 0 ? j : j)  << "\n";
+        if (l + (j > 0 ? j : j) > 23) {
+        //if(++l >= 22 || l + j >= 22) {
+          if (debug)  cout << "son the bit, l: " << l << ", j: " << j << "\n";
           break;
         }
         if (quebrado_i2 >= lim) {
+          if (debug)  cout << "one\n";
           if (!fl_primeiro && !offset)  {
+          if (debug)  cout << "two\n";
             offset = -l + j; // j < 0
-            cout << "j: " << j << "\n";
+            if (debug)  cout << "j: " << j << "\n";
           }
+          if (debug)  cout << "tre\n";
           fl_primeiro = true;
+          if (debug)  cout << "for\n";
+          if (debug)  cout << "quebrado_i2: " << quebrado_i2 << ", lim: " << lim << "\n";
           quebrado_i2 %= lim;
-          cout << "vai um, agora quebrado: " << quebrado_i2 << "\n";
+          if (debug)  cout << "fiv\n";
+          if (debug)  cout << "vai um, agora quebrado: " << quebrado_i2 << "\n";
           a += (1 << (23 - l - offset + (j > 0 ? 0 : j))) & ((1 << 23) - 1);
-          cout << "o shift disso: " << "1 << " << (23 - l - offset + (j > 0 ? 0 : j))<< "\n";
-          cout << "offset: " << offset << "\n";
+          if (debug)  cout << "o shift disso: " << "1 << " << (23 - l - offset + (j > 0 ? 0 : j))<< "\n";
+          if (debug)  cout << "offset: " << offset << "\n";
         } else {
           if (j <= 0 && !fl_primeiro) --j;
         }
-        cout << quebrado_i2 << " < " << lim << "\n";
+        if (debug)  cout << quebrado_i2 << " < " << lim << "\n";
       }
-      if (j <= 0) --j;
-    cout << "j: " << j << "\n";
+      if (j < 0) --j;
+    if (debug)  cout << "j: " << j << "\n";
 
     }
 
     // calcula o expoente
-    cout << "expoente: " << j << "\n";
-    a += ((j + 127) & 0b11111111) << 23;
+    if (debug)  cout << "expoente: " << j << "\n";
+
+    if (a != 0 && j != 0) a += ((j + 127 ) & 0b11111111) << 23;
     //a += ((j) & 0b11111111) << 23;
 
 
 
      
     
-    cout << "\n-----\n";
-    cout << "se     1.s                     .\n";
-    cout << bitset <32> (a) << "\n";
-  }
-
-
-
-  cout << "\n\nfim\n\n";
-  return 0;
-
-
-
-
+    if (debug)  cout << "\n-----\n";
+    if (debug)  cout << "se     1.s                     .\n";
+    cout << "\n\n" << "(input): " << input << "\n"; 
+    cout << "(binar): " << bitset <32> (a) << "\n";
+    //if (debug)  cout << (float) a << "\n";
+    if (debug) cout << "\n\n-------------------------------\n\n";
+    return (float) a;
 }
+
+
+
+int main() {
+
+  cout << "(float): "<< converteNum("123456789");
+  cout << "(float): "<<converteNum("123456.123456");
+  cout << "(float): "<<converteNum(".00000001");
+  cout << "(float): "<<converteNum("00.00");
+  
+  return 0;
+}
+
+
+

c6e869ce644e4ee92b8d2c0193eb18b0b2fd17ec
pracb <pracb@pracb-VirtualBox.(none)>
2014-09-25 13:53:49 +0900

    Q4

---
+++ exJava/Q4.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class Q4 {
+
+  public static void main (String args[]) {
+    System.out.print("\nPlease input one string: "); 
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String line;
+    char line1[], line2[];
+    int l1, l2;
+    try {
+     
+      line = reader.readLine();
+      line1 = line.toCharArray();
+      l1 = line.length();
+      
+      System.out.print("\nPlease input another string: "); 
+      line = reader.readLine();
+      line2 = line.toCharArray();
+      l2 = line.length();
+
+      int j = 0;
+      for (int i = 0; i < l1 - l2 + 1; i++) {
+        for (j = 0; j < l2; j++) {
+          if (line1[i + j] != line2[j]) break;
+        }
+        if (j == l2) {
+          System.out.print("Substring located @ ");
+          System.out.println(i);
+          break;
+        }
+      }
+
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
+    }
+    
+  }
+
+}

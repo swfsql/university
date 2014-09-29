94d5e8191d16c7af9f555bdb199688d7afd18eee
Thiago Machado <thy@hotmail.com.br>
2014-09-29 23:43:33 +0900

    Q23 ok

--- exJava/quiz2/Q23.java
+++ exJava/quiz2/Q23.java
@@ -27,31 +27,40 @@ class Q23Main {
 class MultiCollection {
 
   ReverseList rlist = new ReverseList();
+  HashList hlist = new HashList();
 
   public void addList(Object str) {
     rlist.add(str);
   }
 
   public void addMap(Object key, Object value) {
-
-
+    hlist.add(key, value);
   }
 
   public void showAll() {
     System.out.println("## List ##");
-    showList(rlist.head);
+    showReverseList(rlist.head);
 
     System.out.println("## Map ##");
-
+    for (int i = 0; i < hlist.L; i++) {
+      showHashList(hlist.heads[i]);
+    }
   }
 
-  public void showList(Node n) {
+  // recursive, prints objects from reverseList in FIFO order.
+  public void showReverseList(Node n) {
     if (n == null) return;
-    showList(n.next);
+    showReverseList(n.next);
     System.out.println(n);
   }
-}
 
+  // recursive, print objects value from hashList in FIFO hash order.
+  public void showHashList(HashNode n) {
+    if (n == null) return;
+    System.out.println(n.key + " : " + n.value);
+    showHashList(n.next);
+  }
+}
 
 class ReverseList {
   Node head;
@@ -84,3 +93,57 @@ class Node {
   }
 }
 
+class HashList {
+
+  public static final int L = 10;
+  HashNode[] heads;
+
+  HashList() {
+    heads = new HashNode[L];
+  }
+
+  void add(Object key, Object value){
+    HashNode node = new HashNode(key, value);
+    int hash = node.key.hashCode() % L;
+    hash = hash < 0 ? -hash : hash;
+    HashNode head = heads[hash];
+    while(head != null) {
+      if (head.key.equals(node.key)) {
+        head.value = node.value;
+        break;
+      } 
+      head = head.next;
+    }
+    if (head == null) {
+      node.next = heads[hash];
+      heads[hash] = node;
+    }
+  }
+
+  Object get(Object key) {
+    HashNode node = new HashNode(key, null);
+    int hash = node.key.hashCode() % L;
+    hash = hash < 0 ? -hash : hash;
+    HashNode head = heads[hash];
+    while(head != null) {
+      if (head.key.equals(node.key)) {
+        node.value = head.value;
+        break;
+      } 
+      head = head.next;
+    }
+    return node.value;
+  }
+
+}
+
+class HashNode {
+  HashNode next;
+  Object key, value;
+
+  public HashNode(Object key, Object value) {
+    next = null;
+    this.key = key;
+    this.value = value;
+  }
+}

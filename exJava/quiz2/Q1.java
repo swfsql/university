5c699caaaca96b1f1d8b302e55632dc7f15038eb
Thiago Machado <thy@hotmail.com.br>
2014-09-29 14:36:02 +0900

    Q22 ok

--- exJava/quiz2/Q1.java
+++ exJava/quiz2/Q1.java
@@ -1,9 +1,15 @@
 import java.io.*;
 
 public class Q1 {
-
-
   public static void main (String args[]) {
+    new Q21Main(); 
+  }
+}
+
+
+class Q21Main {
+
+  public Q21Main () {
     AbstractBank bank = null;
 
     bank = new MizuhoBank();
@@ -12,27 +18,44 @@ public class Q1 {
     System.out.println(bank.getFee());
     System.out.println();
 
-    bank = new MizuhoBank();
+    bank = new UFJBank();
     System.out.println(bank.getName());
     System.out.println(bank.getMaxDeposit());
     System.out.println(bank.getFee());
     System.out.println();
-
-
   }
 }
 
-
-
-public abstract class AbstractBank {
+abstract class AbstractBank {
   public abstract String getName();
   public abstract int getMaxDeposit();
   public abstract int getFee();
 }
 
+class MizuhoBank extends AbstractBank {
+  public String getName() {
+    return "This is Mizuho Bank";
+  }
+  public int getMaxDeposit() {
+    return 100000;
+  }
+  public int getFee() {
+    return 210;
+  }
+}
 
 
-
+class UFJBank extends AbstractBank {
+  public String getName() {
+    return "UFJ";
+  }
+  public int getMaxDeposit() {
+    return 2000000;
+  }
+  public int getFee() {
+    return 105;
+  }
+}
 
 
 

15c90af7d7892254e149dc3bbcb85af6c3d8d2f9
Thiago Machado <thy@hotmail.com.br>
2014-09-30 22:47:14 +0900

    folder and file created

--- lab/ex01/Main.java
+++ lab/ex01/Main.java
@@ -20,10 +20,10 @@ public class Main {
 
       while((line = br.readLine()) != null) {
 
-        html += line;
+        html += line + "\n";
       }
 
-      System.out.println(html);
+      //System.out.println(html);
 
     } catch (MalformedURLException mue) {
 
@@ -34,6 +34,38 @@ public class Main {
     } finally {
 
     }
+
+
+    File file = new File("output");
+    if (!file.mkdir()) {
+      System.out.println("Error to create output folder");
+      return;
+    }
+    
+    file = new File("output/src");
+    if (!file.mkdir()) {
+      System.out.println("Error to create src folder");
+      return;
+    }
+
+    Writer out;
+    try {
+    out = new BufferedWriter(
+      new OutputStreamWriter(
+        new FileOutputStream("output/index.html"),
+        "UTF-8"));
+    out.write(html);
+    out.close();
+    
+    } catch (FileNotFoundException fnfe) {
+      System.out.println("Error to find file output/index.html @ FileOutputStream");
+    } catch (UnsupportedEncodingException uee) {
+      System.out.println("Error to encode file output/index.html @ OutputStreamWriter");
+
+    } catch (IOException ioe) {
+      System.out.println("Error to write/close file output/index.html");
+    }     
+
 
   }
 

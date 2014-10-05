efafc28393fa9223609940b94bcc19f7b21ef26d
Thiago Machado <thy@hotmail.com.br>
2014-10-05 14:48:17 +0900

    js and img downloading done

--- lab/ex01/Main.java
+++ lab/ex01/Main.java
@@ -8,100 +8,50 @@ import net.htmlparser.jericho.*;
 public class Main {
 
   public static void main(String[] args) throws Exception {
+    downURL("https://www.apple.com/index.html"); // downloads the index
+    Source src = new Source(new File("output/index.html")); // jericho init
 
-    URL url;
-    InputStream is = null;
-    BufferedReader br ;
-
-    String line, html = "";
-
-    br = new BufferedReader(
-      new InputStreamReader(
-        new URL("https://www.apple.com/").openStream()));
-
-    while((line = br.readLine()) != null) {
-      html += line + "\n";
-    }
-
-
-    File file = new File("output");
-    if (!file.mkdir()) {
-      System.out.println("Error to create output folder");
-      return;
-    }
-    
-    /*file = new File("output/src");
-    if (!file.mkdir()) {
-      System.out.println("Error to create src folder");
-      return;
-    }*/
-
-    Writer out;
-    out = new BufferedWriter(
-      new OutputStreamWriter(
-        new FileOutputStream("output/index.html"),
-        "UTF-8"));
-    out.write(html);
-    out.close();
-
-    Source src = new Source(new File("output/index.html"));
-
-    List<Element> el = src.getAllElements(HTMLElementName.LINK);
+    List<Element> el = src.getAllElements(HTMLElementName.LINK); // .css|.rss
     for (Element e : el) {
-      String rel = e.getAttributeValue("rel");
-      String href = e.getAttributeValue("href");
-      String pathf = "", path = "";
-      Pattern p;
-      System.out.println("rel: [" + rel + "], href: [" + href + "]");
-
-      if (rel.equals("stylesheet")) {
-        if (!href.matches("^https?.*$")) {
-          continue;
-        }
-        url = new URL(href);
-        
-        // the dot actually grabs any char
-        pathf = href.replaceFirst("^https?://(www.)?apple.com/(.*)$", "output/$2");
-        path = pathf.replaceFirst("^(.+/)[^/]+$", "$1");
-        file = new File(path);
-        if (file.exists() == false && file.mkdirs() == false) {
-          System.out.println("Error to create [" + path + "] folder.");
-        }
-
-        System.out.println("pathf: [" + pathf + "]");
-        
-        ReadableByteChannel rbc = Channels.newChannel(url.openStream());
-        FileOutputStream fos = new FileOutputStream(pathf);
-        fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-
-        System.out.println("------------");
-
-        // .css
-      } else if (rel.equals("alternate")) {
-
-        // .rss
+      if (e.getAttributeValue("rel").matches("stylesheet|alternate")) {
+        downURL(e.getAttributeValue("href"));
       }
     }
 
-
-    el = src.getAllElements(HTMLElementName.SCRIPT);
+    el = src.getAllElements(HTMLElementName.SCRIPT); // .js
     for (Element e : el) {
-      String a_src = e.getAttributeValue("src");
-      if (!a_src.equals("")) {
-
-      } 
+      downURL(e.getAttributeValue("src"));
     }
 
-    el = src.getAllElements(HTMLElementName.IMG);
+    el = src.getAllElements(HTMLElementName.IMG); // img
     for (Element e : el) {
-      String a_src = e.getAttributeValue("src");
-
+      downURL(e.getAttributeValue("src"));
     }
     
-    
-    
-    System.out.println("===========\n"+src.getCacheDebugInfo());
+    //System.out.println("===========\n"+src.getCacheDebugInfo());
 
+  }
+
+  static void downURL(String href) throws Exception {
+    if (!href.matches("^https?.*$")) { // just to be sure its a link
+      return;
+    }
+
+    // the dot actually grabs any char
+    String pathf = href.replaceFirst("^https?://(www.)?apple.com/(.*)$", "output/$2");
+    String path = pathf.replaceFirst("^(.+/)[^/]+$", "$1");
+
+    File file = new File(path);
+    if (file.exists() == false && file.mkdirs() == false) {
+      System.out.println("Error to create [" + path + "] folder.");
+    }
+
+    URL url = new URL(href);
+    ReadableByteChannel rbc = Channels.newChannel(url.openStream());
+    FileOutputStream fos = new FileOutputStream(pathf);
+    fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
+    
+    System.out.println("file: [" + pathf + "], from: [" + href + "]");
   }
 
 }

bb050b25bc4306859078251129970005262c412c
Thiago Machado <thy@hotmail.com.br>
2014-10-09 02:06:21 +0900

    created a class

--- lab/ex01/Main.java
+++ lab/ex01/Main.java
@@ -8,53 +8,72 @@ import net.htmlparser.jericho.*;
 public class Main {
 
   public static void main(String[] args) throws Exception {
-    String pre = "output/";
-    downURL("https://www.apple.com/index.html", pre); // downloads the index
+    PageDown page = new PageDown("http://www.apple.com/index.html", "offline/", 1, true);
+  }
 
-    Source src = new Source(new File(pre + "index.html")); // jericho init
+}
+ 
+class PageDown {
+
+  private String pre, back, baseReg;
+
+  PageDown(String url, String pre, int depth, boolean cssImg) throws Exception {
+
+    String webExtensions = "com|net|org|co|jp";
+    // "https://www.apple.com/" => "apple.com/" 
+    String base = url.replaceFirst("^https?://(?:www\\.)?(.+?\\.(?:" + 
+      webExtensions + "))/?(?:.*)$", "$1/");
+    // "apple.com/" => "apple\.com/"
+    
+    baseReg = base.replaceAll("\\.", "\\\\.");
+    this.pre = pre;
+
+    // index.html
+    String pathf = downURL(url); // downloads the index
+    
+    // we add a couple of "../" on this pages links
+    back = pathf.replaceAll("(?:^\\w+/)|[^/]", "").replaceAll("/", "../");
+
+    // now we start downloading the page resources
+    Source src = new Source(new File(pathf)); // jericho init
     List<Element> el;
+
+    // .js and img
+    el = src.getAllElements(HTMLElementName.SCRIPT); 
+    el.addAll(src.getAllElements(HTMLElementName.IMG));
+    for (Element e : el) {
+      downURL(e.getAttributeValue("src"));
+    }
 
     // .css & .rss
     {
       // grab every img url inside the .css 
       el = src.getAllElements(HTMLElementName.LINK); 
       for (Element e : el) {
-        if (e.getAttributeValue("rel").matches("stylesheet")) { 
+        if (e.getAttributeValue("rel").matches("alternate")) { // .rss
+          downURL(e.getAttributeValue("href"));
+        } else if (e.getAttributeValue("rel").matches("stylesheet")) { 
           String href = e.getAttributeValue("href");
-          String pathf = downURL(href, pre);
-          imgFromCss(pathf, pre);
-        } else if (e.getAttributeValue("rel").matches("alternate")) { // .rss
-          downURL(e.getAttributeValue("href"), pre);
-        }
-      }
-    }
-
-    // .js
-    el = src.getAllElements(HTMLElementName.SCRIPT); 
-    for (Element e : el) {
-      System.out.println();
-      downURL(e.getAttributeValue("src"), pre);
-    }
-
-    // img
-    el = src.getAllElements(HTMLElementName.IMG); 
-    for (Element e : el) {
-      System.out.println();
-      downURL(e.getAttributeValue("src"), pre);
+          String pathf2 = downURL(href);
+          if (cssImg) {
+            imgFromCss(href, pathf2);
+          }
+        } 
     }
     
-    replaceSave(src.toString(), pre + "index.html", "");
+    replaceSave(src.toString(), pathf, back);
   }
+}
 
-  static String downURL(String href, String pre) throws Exception {
+  String downURL(String href) throws Exception {
     if (!href.matches("^https?.*$")) { // just to be sure its a link
       return "";
     }
 
-    // "http://apple.com/a/b.file" => "output/a/b.file"
+    // "http://site.com/a/b.file" => "offline/site.com/a/b.file"
     String pathf = href.replaceFirst(
-      "^https?://(?:www\\.)?apple\\.com/(.*)$", pre + "$1"); 
-
+      "^https?://(?:www\\.)?(.*)$", pre + "$1"); 
+    
     // "output/a/b.file" => "output/a/"
     String path = pathf.replaceFirst("^(.+/)[^/]+$", "$1"); 
 
@@ -67,35 +86,40 @@ public class Main {
     return pathf;
   }
 
-  static void saveFile(String pathf, InputStream is) throws Exception {
+  void saveFile(String pathf, InputStream is) throws Exception {
     System.out.print("<");
     ReadableByteChannel rbc = Channels.newChannel(is);
     FileOutputStream fos = new FileOutputStream(pathf);
     fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
     fos.close();
+    System.out.print(pathf);
     System.out.print(">");
-    //System.out.println("new file: [" + pathf + "]");
+    System.out.print("\n");
   }
 
-  static void replaceSave(String s, String pathf, String pre) throws Exception {
+  void replaceSave(String s, String pathf, String pre) throws Exception {
     // for relative = 0:
-    // "http://www.apple.com/a.html" => "a.html".
+    // "http://www.site.com/a.html" => "a.html".
     //
     // for relative > 0:
-    // "http://www.apple.com/a/b/c/d.png" 
-    // in the "http://www.apple.com/a/b/e.css => // "../../a/b/c/d.png"
-    // because the img inside the css looks for paths in relation to the .css path
+    // "http://www.site.com/a/b/c/d.png" 
+    // in the "http://www.site.com/a/b/e.css => // "../../a/b/c/d.png"
+    // because the img inside the css looks for 
+    // paths in relation to the .css path
     
-    s = s.replaceAll("\"https?://(?:www\\.)?apple\\.com/(.*?)\"", "\"" + pre + "$1\"");
+    // "http://site.com/a/b.file" => "offline/a/b.file"
+    s = s.replaceAll("\"https?://(?:www\\.)?(.*?)\"", 
+      "\"" + pre + "$1\"");
     saveFile(pathf, new ByteArrayInputStream(s.getBytes()));
   
   }
 
-  static void imgFromCss(String pathf, String pre) throws Exception {
+
+  void imgFromCss(String href, String pathf) throws Exception {
     
     Pattern p = Pattern.compile("image:url\\(\"((?!data:image).*?)\"\\)");
     Matcher m;
-    String path, href = "", css, txt;
+    String path, css, txt;
 
     if (pathf == null || pathf.equals("")) { 
       // when trying to download the font, downURL returns "" 
@@ -108,23 +132,24 @@ public class Main {
      int i = -1;
     while(m.find()) {
       i = 0;
-      txt = m.group(1); // "http://www.apple.com/img.png" or "../a/img.png"
+      txt = m.group(1); // "http://www.site.com/img.png" or "../a/img.png"
        while(txt.matches("^\\.\\./.*$")) { // if "../a/img.png"
         // "../../a/img.png" => "../a/img.png"
         txt = txt.replaceFirst("^\\.\\./(.*)$", "$1"); 
         i++;
       }
        if (i > 0) { // its a relative path
-        // example: we were in "http://www.apple.com/a/b/c.css"
+        // example: we were in "http://www.site.com/a/b/c.css"
         // and we got an img "../c/d.png" inside that .css
         // (actually, now this img is already "c/d.png", and i = 1
-        // then that img => "http://www.apple.com/a/c/d.png"
+        // then that img => "http://www.site.com/a/c/d.png"
         txt = href.replaceFirst("^(.+/)(.+/){" + i + "}[^/]+$", "$1") + txt;  
       } 
-       downURL(txt, pre);
+       downURL(txt);
     }
     
     if (i != -1) {
+
       replaceSave(css.toString(), pathf, 
         pathf.replaceAll("(?:^\\w+/)|[^/]", "").replaceAll("/", "../"));
       // we add a couple of "../" on the non-relatives images 
@@ -132,4 +157,5 @@ public class Main {
 
   } 
 }
- 
+
+

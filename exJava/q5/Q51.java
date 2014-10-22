45a7763b30864f3c6b45c514b19b7879a22bc7e0
Thiago Machado <thy@hotmail.com.br>
2014-10-22 11:37:04 +0900

    q5 done

--- exJava/q5/Q51.java
+++ exJava/q5/Q51.java
@@ -1,20 +1,34 @@
-import java.nio.*;
-import java.nio.channels.*;
-import java.io.*;
+import java.lang.Thread;
 
-public class Q41 {
+public class Q51 {
+  public static void main(String[] args) {
+    Thread th1 = new Thread(new InnerThread("Thread1"));
+    Thread th2 = new Thread(new InnerThread("Thread2"));
+    Thread th3 = new Thread(new InnerThread("Thread3"));
 
-  public static void main(String args[]) throws Exception {
-
-    FileOutputStream fos = new FileOutputStream(args[args.length - 1]);
-    FileChannel fc = fos.getChannel();
-    ReadableByteChannel rbc;
-
-    for(int i = 0; i < args.length - 1; i++) {
-      rbc = Channels.newChannel(new FileInputStream(new File(args[i])));
-      fc.transferFrom(rbc, fc.size(), Long.MAX_VALUE);
-    }
-    fos.close();
+    th1.start();
+    th2.start();
+    th3.start();
   }
 }
+
+class InnerThread implements Runnable {
+  private String name;
+
+  InnerThread(String name) {
+    this.name = name;
+  }
+
+  public void run() {
+    try {
+      for (int i = 0; i < 10; i++) {
+        System.out.println(name + " " + i + " times");
+        Thread.sleep(1000);
+      }
+    } catch(Exception e) {
+
+    }
+  }
+}
+
 

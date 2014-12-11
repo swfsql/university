091a0f0f7b13f63b92deff5a208efe7ebe9190ab
pracb <pracb@pracb-VirtualBox.(none)>
2014-12-11 16:17:04 +0900

    commented

--- exJava/kuru/Game.java
+++ exJava/kuru/Game.java
@@ -6,74 +6,95 @@ import javax.swing.*;
 
 public class Game extends JApplet {
 
-  int 
-      x = 10, y = 10,
-      vx = 22, vy = 22,
-      pax, pay, pbx, pby,
-      len = 100,
-      rot = 0,
-      spin = 12, 
-      thick = 20;
+	static int FPS = 25;
 
-	int camX = 0, 
-			camY = 0;
-	int stageW = 3600, 
-			stageH = 3600;
-  static int 
-      W = 1280,
-      H = 1024;
-  boolean kr, kl, ku, kd;
+  int // BAR 
+      x = 10, y = 10, // middle point position
+      vx = 22, vy = 22, // velocity
+      pax, pay, pbx, pby, // bar extreme points 
+      len = 100, // length
+      rot = 0, // initial rotation
+      spin = 12, // rotation velocity
+      thick = 20; // thickness
+
+	int // CAMERA
+		camX = 0, camY = 0; // position
+	float camSpeed = 1; // 0 < camSpeed <= 1. (1 = full speed)
+  static int // WINDOW
+      W = 1280, H = 1024; // width, height
+
+	int  // STAGE
+		stageW = 3600, stageH = 3600; // width, height
+	// this changes every stage
+
+	// keyboard events trigger booleans:
+  boolean kr, kl, ku, kd; // right, left, up, down
 
   public Game() {
-    addKeyListener(new KeyList());
-    setFocusable(true);
+    addKeyListener(new KeyList()); // keyboard event
+    setFocusable(true); // keyboard focus
   }
 
+	// bar rotation
   public void rotate() {
     rot += spin;
   }
 
+	// camera movement
 	public void camera() {
+		// camera tries to follow the bar
 		camX += (x - camX) * 1;
 		camY += (y - camY) * 1;
+		// camera boundaries
 		camX = camX < W / 2 ? W / 2 : camX + W / 2 > stageW ? stageW - W / 2 : camX;
 		camY = camY < H / 2 ? H / 2 : camY + H / 2 > stageH ? stageH - H / 2 : camY;
 	}
 
+	// bar movement
   public void move() {
+		// keyboard triggered movement
     x += kr ? vx : kl ? -vx : 0;
     y -= ku ? vy : kd ? -vy : 0;
 
+		// bar boundaries (the should not disappear from the screen)
     x = x < len + thick ? len + thick : x > stageW - thick - len ? stageW - thick - len : x;
     y = y < len + thick ? len + thick : y > stageH - thick - len ? stageH - thick - len : y;
 
+		// bar's boundaries coordinate
     pax = x + (int)(len * Math.cos(Math.toRadians(rot)));
     pay = y + (int)(len * Math.sin(Math.toRadians(rot)));
     pbx = x - (int)(len * Math.cos(Math.toRadians(rot)));
     pby = y - (int)(len * Math.sin(Math.toRadians(rot)));
   }
 
-  public void clearBar(Graphics2D g) {
+	// clear drawings
+  public void clear(Graphics2D g) {
     g.clearRect(0,0,W,H);
-    //g.clearRect(x - len - thick/2 - 1 , y - len - thick/2 - 1, x + len + thick/2 + 1, y + len + thick/2 + 1);
   }
 
-  public void paint(Graphics graphics) {
-    Graphics2D g = (Graphics2D) graphics;
-    clearBar(g);
-		drawLevel(g);
-    move();
-		camera();
-    rotate();
-    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+	public void drawBar(Graphics2D g) {
     g.setPaint(Color.gray);
     g.setStroke(new BasicStroke(thick));
     //g.draw(new Line2D.Double(pax, pay, pbx , pby ));
     g.draw(new Line2D.Double(pax - camX + W / 2, pay - camY + H / 2, pbx - camX + W / 2, pby - camY + H / 2));
+	}
+
+  public void paint(Graphics graphics) {
+    Graphics2D g = (Graphics2D) graphics;
+    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+    rotate();
+    move();
+		camera();
+
+		// draw
+    clear(g);
+		drawLevel(g);
+		drawBar(g);
   }
 
   public static void main(String s[]) throws Exception {
-    JFrame f = new JFrame("LineDemo");
+    JFrame f = new JFrame("Kuru");
     Game game = new Game();
     f.add(game);
     f.setSize(W, H);
@@ -82,10 +103,11 @@ public class Game extends JApplet {
 
     while (true) {
       game.repaint();
-      Thread.sleep(40);
+      Thread.sleep(1000 / FPS);
     }
   }
 
+	// will delete later, this is NOT how we will draw levels!
 	public void drawLevel(Graphics2D g) {
 		int []lvl1x = {20, 80, 120, 460};
 		int []lvl1y = {20, 0, 100, 1150};
@@ -103,6 +125,7 @@ public class Game extends JApplet {
 		g.fillPolygon(lvlcpx, lvlcpy, 4);
 	}
 
+	// keyboard events
   class KeyList implements KeyListener {
     public void keyTyped(KeyEvent e) {
 

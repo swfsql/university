204f6dc54a989f3bf9f2b5f67ff8f637ea02340d
Thiago Machado da Silva <swfsql@gmail.com>
2015-01-04 19:40:05 +0900

    started collision work

--- Game.java
+++ Game.java
@@ -14,21 +14,29 @@ public class Game extends JApplet {
 	int // BAR 
 	x = 180, y = 360, // middle point position
 	vx = 22, vy = 22, // velocity
-	pax, pay, pbx, pby, // bar extreme points 
 	len = 80, // length
 	rot = 0, // initial rotation
 	spin = 12, // rotation velocity
-	thick = 20; // thickness
-
+	thick = 20, // thickness
+	// collision
+	collision_points = 5, // how many points are going to test wall collision on each side (excluding the middle point)
+	collided = 0, // a counter
+	// when collided = 0, the is not colliding and is not "recovering" from a collision
+	// when collided > 0, the bar is "recovering" from a collision (so we don't need to test for new collisions)
+	collided_max = 10, // when the bar collides, the counter goes up to this value
+	vx_old, vy_old; // velocity when the collision started
+	
 	int // CAMERA
 	camX = 0, camY = 0; // position
 	float camSpeed = 1; // 0 < camSpeed <= 1. (1 = full speed)
-	static int // WINDOW
-	W = 1280, H = 1024; // width, height
+	int // WINDOW
+	W, H; // width, height
+	static double	
+	window_multiplier = 1; // 0 < window_multiplier <= 1 = fullscreen
 
 	int  // STAGE
-	stageW = 3600, stageH = 3600; // width, height
-	// this changes every stage
+	stageW, stageH; // width, height
+	BufferedImage bimg;
 
 	// keyboard events trigger booleans:
 	boolean kr, kl, ku, kd; // right, left, up, down
@@ -39,15 +47,15 @@ public class Game extends JApplet {
 	Image stage; // loaed stage image in stage folder
 
   public Game() throws Exception {
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		W = (int) (screenSize.width * window_multiplier);
+		H = (int) (screenSize.height * window_multiplier);
+
     addKeyListener(new KeyList()); // keyboard event
     setFocusable(true); // keyboard focus
 		stage = loadStage("stages/test.png");
   }
 
-	// bar rotation
-	public void rotate() {
-		rot += spin;
-	}
 
 	// camera movement
 	public void camera() {
@@ -61,27 +69,71 @@ public class Game extends JApplet {
 
 	// bar movement
 	public void move() {
-		// keyboard triggered movement
-		x += kr ? vx : kl ? -vx : 0;
-		y -= ku ? vy : kd ? -vy : 0;
+
+		if (collided == 0) { // normal keyboard movement
+			vx_old = kr ? vx : kl ? -vx : 0;
+			vy_old = ku ? vy : kd ? -vy : 0;
+			
+			// keyboard triggered movement
+			x += vx_old;
+			y -= vy_old;
+			
+			rot += spin; // bar rotation
+		} else { // we had a collision
+
+			x -= vx_old * collided / collided_max;
+			y += vy_old * collided / collided_max;
+
+			rot -= spin; // bar backwards rotation
+		}
 
 		// bar boundaries (the should not disappear from the screen)
 		x = x < len + thick ? len + thick : x > stageW - thick - len ? stageW - thick - len : x;
 		y = y < len + thick ? len + thick : y > stageH - thick - len ? stageH - thick - len : y;
 
-		// bar's boundaries coordinate
-		pax = x + (int)(len * Math.cos(Math.toRadians(rot)));
-		pay = y + (int)(len * Math.sin(Math.toRadians(rot)));
-		pbx = x - (int)(len * Math.cos(Math.toRadians(rot)));
-		pby = y - (int)(len * Math.sin(Math.toRadians(rot)));
 	}
+
+	public void collision() {
+		if (collided > 0) {
+			collided--;
+			return;
+		}
+
+		double rot_rad = Math.toRadians(rot);
+		float len2 = len / collision_points;
+		boolean new_collision = test_collision(x, y);
+		for (int i = 0; i < collision_points && !new_collision; i++) {
+			new_collision = test_collision(x + (int)(len2 * (i + 1) * Math.cos(rot_rad)), y + (int)(len2 * (i + 1) * Math.sin(rot_rad)));
+		}
+		for (int i = 0; i < collision_points && !new_collision; i++) {
+			new_collision = test_collision(x - (int)(len2 * (i + 1) * Math.cos(rot_rad)), y - (int)(len2 * (i + 1) * Math.sin(rot_rad)));
+		}
+		if (new_collision) {
+			collided = collided_max;
+			//System.out.println("collided");
+		}
+
+	}
+	public boolean test_collision(int i, int j) {
+		return bimg.getRGB(i, j) != -1; 
+	}
+
+	
 
 	// clear drawings
 	public void clear() {
+		// clears the entire screen
 		gv.clearRect(0, 0, W, H);
 	}
 
 	public void drawBar() {
+		// bar's boundaries coordinate
+		double rot_rad = Math.toRadians(rot);
+		int 
+			pax = x + (int)(len * Math.cos(rot_rad)),
+			pay = y + (int)(len * Math.sin(rot_rad)),
+			pbx = x - (int)(len * Math.cos(rot_rad)),
+			pby = y - (int)(len * Math.sin(rot_rad));
 		((Graphics2D) gv).setPaint(Color.gray);
 		((Graphics2D) gv).setStroke(new BasicStroke(thick));
 		//g.draw(new Line2D.Double(pax, pay, pbx , pby ));
@@ -99,7 +151,7 @@ public class Game extends JApplet {
 		//Graphics gv = offImage.getGraphics();
 		((Graphics2D) gv).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-		rotate();
+		collision();
 		move();
 		camera();
 
@@ -112,28 +164,16 @@ public class Game extends JApplet {
 		graphics.drawImage(offImage, 0, 0, W, H, this);
   }
 
-  public static void main(String s[]) throws Exception {
-    JFrame f = new JFrame("Kuru");
-    Game game = new Game();
-    f.add(game);
-    f.setSize(W, H);
-    f.setVisible(true);
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    while (true) {
-      game.repaint();
-      Thread.sleep(1000 / FPS);
-    }
-  }
 
 	public Image loadStage(String filename) throws Exception {
 		Image img = ImageIO.read(new File(filename));
-		BufferedImage bimg = (BufferedImage) img;
+		bimg = (BufferedImage) img;
  		stageW = bimg.getWidth();
 		stageH = bimg.getHeight();
 		return img;
 	}
 
-	// will delete later, this is NOT how we will draw levels!
+	// stage drawing
 	public void drawStage() {
 		((Graphics2D) gv).drawImage(stage, W / 2 - camX, H / 2 - camY, this);
 	}
@@ -145,34 +185,45 @@ public class Game extends JApplet {
 		}
 		public void keyPressed(KeyEvent e) {
 			String key = KeyEvent.getKeyText(e.getKeyCode());
-			if (key.equals("Right") || key.equals("右")) {
+			if (key.equals("Right") || key.equals("右") || key.equals("→")) {
 				kr = true;
-			} else if (key.equals("Left")|| key.equals("左")) {
+			} else if (key.equals("Left")|| key.equals("左") || key.equals("←")) {
 				kl = true;
 			}
-			if (key.equals("Up")|| key.equals("上")) {
+			if (key.equals("Up")|| key.equals("上") || key.equals("↑")) {
 				ku = true;
-			} else if (key.equals("Down")|| key.equals("下")) {
+			} else if (key.equals("Down")|| key.equals("下") || key.equals("↓")) {
 				kd = true;
 			}
 		}
 		public void keyReleased(KeyEvent e) {
 			String key = KeyEvent.getKeyText(e.getKeyCode());
-			if (key.equals("Right") || key.equals("右")) {
+			if (key.equals("Right") || key.equals("右") || key.equals("→")) {
 				kr = false;
-			} else if (key.equals("Left")|| key.equals("左")) {
+			} else if (key.equals("Left")|| key.equals("左") || key.equals("←")) {
 				kl = false;
 			}
-			if (key.equals("Up")|| key.equals("上")) {
+			if (key.equals("Up")|| key.equals("上") || key.equals("↑")) {
 				ku = false;
-			} else if (key.equals("Down")|| key.equals("下")) {
+			} else if (key.equals("Down")|| key.equals("下") || key.equals("↓")) {
 				kd = false;
 			}
 		}
 
 	}
 
+  public static void main(String s[]) throws Exception {
+    JFrame f = new JFrame("Kuru");
+    Game game = new Game();
+    f.add(game);
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		f.setBounds(0, 0, (int) (screenSize.width * window_multiplier), (int) (screenSize.height * window_multiplier));
+    f.setVisible(true);
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    while (true) {
+      game.repaint();
+      Thread.sleep(1000 / FPS);
+    }
+  }
+
 }
-
-
-
